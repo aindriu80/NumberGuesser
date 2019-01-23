567980a8e088d2e11c09420a5c9f871d79876559
aindriu80 <aindriu80@gmail.com>
2019-01-23 16:49:46 +0000

    Play again functionality

--- resources/js/script.js
+++ resources/js/script.js
@@ -21,6 +21,13 @@ const game = document.querySelector("#game"),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+game.addEventListener("mousedown", function(e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+
 // Listen for guess
 guessBtn.addEventListener("click", function() {
   let guess = parseInt(guessInput.value);
@@ -76,6 +83,10 @@ function gameOver(won, msg) {
 
   // Set message
   setMessage(msg);
+
+  // Play Again ??
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again";
 }
 
 // Set message
